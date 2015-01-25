81b6b85731c49c93da56c9628b1af61a8a18011e
Sandeep J <jsanweb@gmail.com>
2015-01-25 09:17:56 +0000

    Make models.

--- fabfile.py
+++ fabfile.py
@@ -5,6 +5,14 @@ def push(msg, remote = "origin", branch = "master"):
   local("git commit -m '%s'" %(msg))
   local("git push %s %s" %(remote, branch))
 
+def db(name = "db.sqlite3"):
+  """
+  Delete the (sqlite3) database and create a new one.
+  """
+  local("rm -rf %s" %(name))
+  local("python manage.py makemigrations")
+  local("python manage.py migrate")
+
 def run(port = 8888):
   local("python manage.py runserver 0.0.0.0:%s" %(port))
 
--- tweets/models.py
+++ tweets/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Tweet(models.Model):
+  """
+  A simple tweet model.
+  """
+  user = models.ForeignKey(settings.AUTH_USER_MODEL, unique = True)
+  text = models.CharField(max_length = 140, blank = False)
+  timestamp = models.DateTimeField(auto_now_add = True)
+  
+  def __unicode__(self):
+    return "%s:%s" %(self.user.username[:24], self.text[:24])
